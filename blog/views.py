4b547a20bdd83d94b35c43d7f805ad0629ace3c7
cascherman <c.ascherman@gmail.com>
2025-01-27 02:00:44 -0500

    big upload

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,10 @@
 from django.shortcuts import render
-
-
-
-
-# Create your views here.
-
-posts = [
-    {
-        'author': 'Chris',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': "Dec 17, 2024",
-    },
-    {
-        'author': 'Fred',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': "Dec 18, 2024",
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
--- dj/settings.py
+++ dj/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -84,7 +87,10 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-#admin password is testing123
+
+
+#admin name: chris
+#admin password: testing123
 
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -119,8 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
